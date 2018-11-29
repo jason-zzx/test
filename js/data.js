757fad0b0070f4278970c119e9bcbc133f878b79
jason-zzx <43471058+jason-zzx@users.noreply.github.com>
2018-11-29 14:32:50 +0800

    Update data.js

--- js/data.js
+++ js/data.js
@@ -1,21 +1,28 @@
 var json = [];
-//var threeSize = new Array(52);
 var threeSize = new Array(
   {B:'83',W:'56',H:'82'},
   {B:'72',W:'52',H:'78'},
   {B:'86',W:'55',H:'83'},
   {B:'81',W:'56',H:'81'},
   {B:'74',W:'54',H:'78'},
-  {B:'75',W:'57',H:'78'}
+  {B:'75',W:'57',H:'78'},
+  {B:'77',W:'54',H:'79'},
+  {B:'90',W:'62',H:'92'}
 );
 //threeSize[1] = {B:'72',W:'52',H:'78'}; 
-// var idolName = new Array(
-//   "天海春香",
-  
-// );
-// var idolValue = new Array(
-//   "haruka",
-// );
+var idolName = new Array(
+  "天海春香",
+  "如月千早",
+  "星井美希",
+  "萩原雪歩",
+  "高槻やよい",
+  "菊地真",
+  "水瀬伊織",
+  "四条貴音"
+);
+var idolValue = new Array(
+  "haruka",
+);
 for(x in threeSize){
   json.push(threeSize[x]);
 }
