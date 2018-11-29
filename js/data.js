42e4a1c29ac47d7ea0f2b011297b77f4db458920
jason-zzx <43471058+jason-zzx@users.noreply.github.com>
2018-11-29 14:21:47 +0800

    Update data.js

--- js/data.js
+++ js/data.js
@@ -1,6 +1,14 @@
 var json = [];
-var threeSize = new Array(52);
-threeSize[1] = {B:'83',W:'56',H:'82'}; 
+//var threeSize = new Array(52);
+var threeSize = new Array( {B:'83',W:'56',H:'82'}, {B:'72',W:'52',H:'78'});
+//threeSize[1] = {B:'72',W:'52',H:'78'}; 
+// var idolName = new Array(
+//   "天海春香",
+  
+// );
+// var idolValue = new Array(
+//   "haruka",
+// );
 json.push(threeSize[1]);
 var objs = eval(json);
 alert(objs[0].W);
