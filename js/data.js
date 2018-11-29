8898765979865401035fcb3585d60de77828ed7f
jason-zzx <43471058+jason-zzx@users.noreply.github.com>
2018-11-29 14:25:52 +0800

    Update data.js

--- js/data.js
+++ js/data.js
@@ -1,6 +1,13 @@
 var json = [];
 //var threeSize = new Array(52);
-var threeSize = new Array( {B:'83',W:'56',H:'82'}, {B:'72',W:'52',H:'78'});
+var threeSize = new Array(
+  {B:'83',W:'56',H:'82'},
+  {B:'72',W:'52',H:'78'},
+  {B:'86',W:'55',H:'83'},
+  {B:'81',W:'56',H:'81'},
+  {B:'74',W:'54',H:'78'},
+  {B:'75',W:'57',H:'78'}
+);
 //threeSize[1] = {B:'72',W:'52',H:'78'}; 
 // var idolName = new Array(
 //   "天海春香",
@@ -9,6 +16,8 @@ var threeSize = new Array( {B:'83',W:'56',H:'82'}, {B:'72',W:'52',H:'78'});
 // var idolValue = new Array(
 //   "haruka",
 // );
-json.push(threeSize[1]);
+for(x in threeSize){
+  json.push(x);
+}
 var objs = eval(json);
 alert(objs[0].W);
