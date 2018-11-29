d48924009411bcfd9b81de59b3bb0284f0744fdf
jason-zzx <43471058+jason-zzx@users.noreply.github.com>
2018-11-29 15:04:15 +0800

    Update myScript.js

--- js/myScript.js
+++ js/myScript.js
@@ -1,5 +1,26 @@
 var objs = eval(json);
-var n = Math.floor(Math.random()*100) % 8;
+var t = objs.length;
+var n = Math.floor(Math.random()*100) % t;
+
 $(".b-field").html(objs[n].B);
 $(".w-field").html(objs[n].W);
 $(".h-field").html(objs[n].H);
+
+var optionNumbers = new Array(n,-1,-1,-1);
+var i = 1;
+
+while(i<5){
+  var tmp = Math.floor(Math.random()*100) % t;
+  var valid = true;
+  for(int j = 0; j < i; j++){
+    if(optionNumbers[j] == tmp){
+      valid = false;
+      break;
+    }
+  }
+  if(valid){
+    optionNumbers[i] = tmp;
+    i++;
+  }
+}
+alert(optionNumbers);
