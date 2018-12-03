8882d75f64edffa12d417c42fa2d9761f7290f58
jason-zzx <43471058+jason-zzx@users.noreply.github.com>
2018-12-03 09:56:55 +0800

    Update testScript.js

--- js/testScript.js
+++ js/testScript.js
@@ -1,1 +1,17 @@
-alert("no");
+var json = [];
+var threeSize = new Array(
+  {B:'83',W:'56',H:'82'}
+);
+//threeSize[1] = {B:'72',W:'52',H:'78'}; 
+var idolName = new Array(
+  "天海春香"
+);
+var idolValue = new Array(
+  "haruka",
+);
+for(x in threeSize){
+  json.push(threeSize[x]);
+}
+
+
+alert("no2");
