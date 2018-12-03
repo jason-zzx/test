7890a43127b4dea09dd930e4b5157ae090c15460
jason-zzx <43471058+jason-zzx@users.noreply.github.com>
2018-12-03 11:50:47 +0800

    Update myScript.js

--- js/myScript.js
+++ js/myScript.js
@@ -1,71 +1,70 @@
 
 var objs = eval(json);
 var t = objs.length;
-var n = Math.floor(Math.random()*100) % t;
+function generateQuestion(){
+    var n = Math.floor(Math.random()*100) % t;
 
-// document.querySelector(".b-field").innerHTML=objs[n].B;
-// document.querySelector(".w-field").innerHTML=objs[n].W;
-// document.querySelector(".h-field").innerHTML=objs[n].H;
-$(".b-field").html(objs[n].B);
-$(".w-field").html(objs[n].W);
-$(".h-field").html(objs[n].H);
+    $(".b-field").html(objs[n].B);
+    $(".w-field").html(objs[n].W);
+    $(".h-field").html(objs[n].H);
 
-var correctPosition = Math.floor(Math.random()*100) % 4;
-var optionNumbers = new Array(-1,-1,-1,-1);
-optionNumbers[correctPosition] = n;
-var i = 0;
-var answered = false;
+    var correctPosition = Math.floor(Math.random()*100) % 4;
+    var optionNumbers = new Array(-1,-1,-1,-1);
+    optionNumbers[correctPosition] = n;
+    var i = 0;
+    var answered = false;
 
-while(i<4){
-    if(optionNumbers[i] != -1){
-        i++;
-        continue;
-    }
-    var tmp = Math.floor(Math.random()*100) % t;
-    while(tmp==n){
-        tmp = Math.floor(Math.random()*100) % t;
-    }
-    var valid = true;
-    if(i==0){
-        optionNumbers[0] = tmp;
-        i++;
-        continue;
-    }
-    for(var j = 0; j < i; j++){
-        if(optionNumbers[j] == tmp){
-            valid = false; 
-            break;
+    while(i<4){
+        if(optionNumbers[i] != -1){
+            i++;
+            continue;
+        }
+        var tmp = Math.floor(Math.random()*100) % t;
+        while(tmp==n){
+            tmp = Math.floor(Math.random()*100) % t;
+        }
+        var valid = true;
+        if(i==0){
+            optionNumbers[0] = tmp;
+            i++;
+            continue;
+        }
+        for(var j = 0; j < i; j++){
+            if(optionNumbers[j] == tmp){
+                valid = false; 
+                break;
+            }
+        }
+        if(valid){
+            optionNumbers[i] = tmp;
+            i++;
         }
     }
-    if(valid){
-        optionNumbers[i] = tmp;
-        i++;
+
+    for(var i = 0;i<4;i++){
+        $(".answer-area:eq("+i+")").html(idolName[optionNumbers[i]]);
+        if(i==correctPosition){
+            $(".answer-area:eq("+i+")").addClass("answer-correct");
+        }else{
+            $(".answer-area:eq("+i+")").addClass("answer-wrong");
+        }
     }
-}
 
-//alert(optionNumbers+"  correct:"+correctPosition);
-
-for(var i = 0;i<4;i++){
-    $(".answer-area:eq("+i+")").html(idolName[optionNumbers[i]]);
-    if(i==correctPosition){
-        $(".answer-area:eq("+i+")").addClass("answer-correct");
-    }else{
-        $(".answer-area:eq("+i+")").addClass("answer-wrong");
-    }
-}
-
-$(".answer-correct").click(function(){
-    if(!answered){
-    answered = true;
-        $(".answer-correct").addClass("correct-answered");
-    }
-});
-
-$(".answer-wrong").click(function(){
-    if(!answered){
+    $(".answer-correct").click(function(){
+        if(!answered){
         answered = true;
-        $(this).addClass("wrong-answered");
-        $(".answer-correct").addClass("correct-answered");
-    }
-//     window.location.reload();
-});
+            $(".answer-correct").addClass("correct-answered");
+        }
+    });
+
+    $(".answer-wrong").click(function(){
+        if(!answered){
+            answered = true;
+            $(this).addClass("wrong-answered");
+            $(".answer-correct").addClass("correct-answered");
+        }
+    //     window.location.reload();
+    });
+}
+
+generateQuestion();
