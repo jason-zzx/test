bc7e95be74c474479b2554acf61e4590e81ae649
jason-zzx <43471058+jason-zzx@users.noreply.github.com>
2018-12-03 10:03:00 +0800

    Update testScript.js

--- js/testScript.js
+++ js/testScript.js
@@ -1,9 +1,11 @@
 var json = [];
 var threeSize = new Array(
+  {B:'83',W:'56',H:'82'},
   {B:'83',W:'56',H:'82'}
 );
 //threeSize[1] = {B:'72',W:'52',H:'78'}; 
 var idolName = new Array(
+  "天海春香",
   "天海春香"
 );
 var idolValue = new Array(
@@ -12,6 +14,7 @@ var idolValue = new Array(
 for(x in threeSize){
   json.push(threeSize[x]);
 }
+var objs = eval(json);
+var t = objs.length;
 
-
-alert("no3");
+alert(t+"4");
